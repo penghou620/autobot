22ae68b6b0c1a354553543c354e67dc12361d940
Peng Hou <dustsnow@gmail.com>
2014-04-23 18:14:29 -0400

    joystick is sort of working except a problem. When user press button 5 and control the robot, if user release button 5 before reset the axe to 0 position, the motor will continue running

--- src/joy_controller.cpp
+++ src/joy_controller.cpp
@@ -7,87 +7,81 @@
 
 ros::Subscriber joy_sub;
 ros::Publisher cmd_vel_pub;
+ros::Publisher estop_pub;
+int prev_button_7 = 0;
 
 void joy_callback(const sensor_msgs::Joy &joy){
 	std_msgs::String result;
-	int button_1 = 1;
-	button_1 = joy.buttons[5];
-	int axe_1 = (int)(joy.axes[1]*100);//forward or backward
-	int axe_3 = (int)(joy.axes[3]*100);//turn left or right
+	int button_7 = (int)(joy.buttons[7]);
+	if(button_7 == 1){
+		std_msgs::String estop_clear;
+		estop_clear.data = "clear";
+		estop_pub.publish(estop_clear);
+	}
+	int button_1 = joy.buttons[5];
+	float axe_1 = (joy.axes[1]);//forward or backward
+	float axe_3 = (joy.axes[3]);//turn left or right
+	int Vmax = 0x1f;
+	//printf("axe_1:%f\n",axe_1);
+	//printf("velocity:%f\n",(Vmax*axe_1));
+	//printf("velocity:%x\n",(int)(Vmax*axe_1));
+
+	//printf("joy axe_1:%f\n",joy.axes[1]);
+	//printf("axe_1:%d\n",axe_1);
+	//printf("joy axe_3:%f\n",joy.axes[3]);
+	//printf("axe_3:%d\n",axe_3);
+	char command_string[7];
 	if(button_1 == 1){
+		prev_button_7 = 1;
 		if(axe_1 > 0.0){
-			result.data = "A10B10";
+			int velocity = (int)(Vmax * axe_1);
+			if(velocity < 16){
+				//velocity = 0;
+				sprintf(command_string,"A0%xB0%x",velocity,velocity);
+			}else{
+				printf("velocity:%x\n",velocity);
+				sprintf(command_string,"A%xB%x",velocity,velocity);
+			}
+			printf("command_string %s\n",command_string);
+			result.data = command_string;
+
 		}
 		else if(axe_1 < 0.0){
-			result.data = "a10b10";
+			int velocity = (int)(Vmax * fabs(axe_1));
+			if(velocity < 16){
+				//velocity = 0;
+				sprintf(command_string,"a0%xb0%x",velocity,velocity);
+			}else{
+				printf("velocity:%x\n",velocity);
+				sprintf(command_string,"a%xb%x",velocity,velocity);
+			}
+			printf("command_string %s\n",command_string);
+			result.data = command_string;
 		}
 		else if(axe_3 > 0.0){
-			result.data = "A10B20";
+			result.data ="A05B10";
 		}
 		else if(axe_3 < 0.0){
-			result.data = "A20B10";
+			result.data = "A10B05";
 		}else{
 			result.data = "A00B00";
 		}
-
 		cmd_vel_pub.publish(result);
+	}else{
+		if(prev_button_7 == 1){
+			prev_button_7 = 0;
+			result.data = "A00B00";
+			cmd_vel_pub.publish(result);
+		}
 	}
-//	float theta = atan2(-1*(msg->x),msg->z);//our model has the opposite coordinates frame with the kinect frame, so negative the x value
-//	printf("theta:%f\n",theta);
-//	printf("head x %f\n",msg->x);
-//	printf("head y %f\n",msg->y);
-//	printf("head z %f\n",msg->z);
-//	std_msgs::String result;
-//	if(fabs(theta) >= 0.5){
-//		printf("Out of the range of the kinect\n");
-//		result.data = "A00B00";
-//		printf("velocity A00 B00\n");
-//	} else {
-//		if(msg->z <= 1500){
-//			printf("in the 1.5m\n");
-//			result.data = "A00B00";
-//			printf("velocity A00 B00\n");
-//		}else{
-//			if(fabs(theta) > 0.05){
-//				printf("Turn\n");
-//				float k=10.0;
-//				//float Vmax = 127.0;
-//				float Vmax = 15.0;
-//				int left_vel = -1 * k*theta + Vmax/2;
-//				int right_vel = Vmax/2 + k*theta;
-//				char tmp[7];
-//				if(left_vel < 16 && right_vel > 16){
-//					sprintf(tmp,"A0%xB%x",left_vel, right_vel);
-//				}else if(right_vel < 16 && left_vel > 16){
-//					sprintf(tmp,"A%xB0%x",left_vel, right_vel);
-//				}else if(left_vel < 16 && right_vel < 16){
-//					sprintf(tmp,"A0%xB0%x",left_vel, right_vel);
-//				}else{
-//					sprintf(tmp,"A%xB%x",left_vel, right_vel);
-//				}
-//				printf("Velocity Command %s\n",tmp);
-//				std::string tmp2(tmp);
-//				result.data = tmp2;
-//			} else {
-//				printf("Move Straight\n");
-//				result.data = "A15B15";
-//				printf("velocity A10 B10\n");
-//			}
-//		}
-//	}
-//
-//	cmd_vel_pub.publish(result);
 }
 int main(int argc, char **argv){
 	ros::init(argc, argv, "joy_controller");
 	ros::NodeHandle nh;
-	joy_sub = nh.subscribe("joy",1,joy_callback);
+	joy_sub = nh.subscribe("joy",1000,joy_callback);
 	cmd_vel_pub = nh.advertise<std_msgs::String>("cmd_vel",1);
-	ros::Rate loop_rate(60);
-	while(ros::ok()){
-		ros::spinOnce();
-		loop_rate.sleep();
-	}
+	estop_pub = nh.advertise<std_msgs::String>("estop",1);
+	ros::spin();
 	return 0;
 }
 
